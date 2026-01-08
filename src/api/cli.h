837e63ea10680f61512ea95806c1abe51490891a
Finn Willems <fintur@fedora.fritz.box>
2026-01-08 14:19:26 +0100

    CLI rework

--- src/api/cli.h
+++ src/api/cli.h
@@ -12,102 +12,109 @@
 #include "../reader/reader.h"
 #include "config.h"
 
-class cli {
-
-    public:
-    ~cli() {
-        delete(con.cam);
-        delete(con.scene);
-    }
+namespace cli {
 
     int run(int argc, char* argv[]){
+
+        config::cli_config con;
+
         std::string command = argv[1];
         std::transform(command.begin(), command.end(), command.begin(), ::tolower);
         if (dispatch_map.contains(command)) {
-            return dispatch_map.at(command)(argc, argv);
+            return dispatch_map.at(command)(argc, argv, con);
         } else {
             std::cerr << "[Error] Unknown command: " << command << std::endl;
-            help();
+            intern::help();
             return 1;
         }
-    }
+    };
 
+    bool clear(config::cli_config &con){
+        delete(con.cam);
+        delete(con.r_renderer);
+        delete(con.scene);
+    };
 
-    
-    private:
-
-    config::cli_config con;
-
-    std::map<std::string, std::function<int(int args, char* argv[])>> dispatch_map = {
-        {"-help", [this](int args, char* argv[]) {
-            help();
+    static const std::map<std::string, std::function<int(int args, char* argv[], config::cli_config &con)>> dispatch_map = {
+        {"-help", [&](int args, char* argv[], config::cli_config &con) {
+            intern::help();
             return 0;
         }},
-        {"-scene--display", [this](int args, char* argv[]) {
+        {"-scene--display", [&](int args, char* argv[], config::cli_config &con) {
             if(args != 1) return 1;
-            return display_scene();
+            return intern::display_scene(con.scene);
         }},
-        {"-render", [this](int args, char* argv[]){
-            //if(args != 2) return 1;
-            return render(argv);
+        {"-render", [&](int args, char* argv[], config::cli_config &con){
+            if(args != 2) return 1;
+            return intern::render(argv, con);
+        }},
+        {"-run--test", [&](int args, char* argv[], config::cli_config &con){
+            if(args != 4) return 1;
+            return intern::run_test(args, argv, con.scene);
         }}
     };
 
-    //Functions
+    namespace intern {
 
-    void help(){
-        std::cout << "RayTracer Usage: "<<
-        "\nRayTracer -render <dst>" << 
-        "\nRayTracer -scene--display" <<
-        "\n                 --erase" <<
-        "\nRayTracer -camera--display" <<
-        "\n                 --erase" <<
-        "\nRayTracer -help";
-    }
+        //Functions
 
-    int initialize(){
-        try{
-            if(!reader::read_config(con)){
-                std::clog << "\n[Error] Could not read config.trt!";
+        void help(){
+            std::cout << "RayTracer Usage: "<<
+            "\nRayTracer -render <dst>" << 
+            "\nRayTracer -run--test <renderer> <renderer>"
+            "\nRayTracer -scene--display" <<
+            "\nRayTracer -camera--display" <<
+            "\nRayTracer -help";
+        };
+
+        int initialize(config::cli_config &con){
+            try{
+                if(!reader::read_config(con)){
+                    std::clog << "\n[Error] Could not read config.trt!";
+                    return 1;
+                } 
+                con.initialized = true;
+                return 0;
+            }catch(...){
+                std::clog << "\n[Error] Could not read Initialize!";
                 return 1;
-            } 
-            this->con.initialized = true;
+            }
+
+        };
+
+        int render(char* argv[], config::cli_config &con){
+            intern::initialize(con);
+            if(!con.initialized){
+                std::clog << "[Error] RayTracer is not initialized!\n";
+                return 1;
+            }
+            try{
+                con.r_renderer->start_rendering(*con.scene);
+                std::ofstream of(argv[2]);
+                if(!of.is_open()) std::clog << "[Error] Couldnt open dst path!\n";
+                con.r_renderer->print_rgbs(of);
+                of.close();
+            }catch(...){
+                std::clog << "[Error] Oops, something went wrong!\n";
+            }
             return 0;
-        }catch(...){
-            std::clog << "\n[Error] Could not read Initialize!";
-            return 1;
-        }
+        };
 
-    }
+        int display_scene(hittable* scene){
+            hittable* scene = reader::intern::read_scene("default_scene");
+            if(scene == nullptr){
+                std::clog << "[Error] Could not read default scene!\n";
+                return 1;
+            }
+            for(auto& obj : static_cast<hittable_list*>(scene)->objects){
+                std::clog << obj << "\n";
+            }
+            return 0;
+        };
 
-    int render(char* argv[]){
-        initialize();
-        if(!this->con.initialized){
-            std::clog << "[Error] RayTracer is not initialized!\n";
-            return 1;
-        }
-        try{
-            this->con.r_renderer->start_rendering(*this->con.scene);
-            std::ofstream of(argv[2]);
-            if(!of.is_open()) std::clog << "[Error] Couldnt open dst path!\n";
-            this->con.r_renderer->print_rgbs(of);
-            of.close();
-        }catch(...){
-            std::clog << "[Error] Oops, something went wrong!\n";
-        }
-        return 0;
-    }
-
-    int display_scene(){
-        hittable* scene = reader::intern::read_scene("default_scene");
-        if(scene == nullptr){
-            std::clog << "[Error] Could not read default scene!\n";
-            return 1;
-        }
-        for(auto& obj : static_cast<hittable_list*>(scene)->objects){
-            std::clog << obj << "\n";
-        }
-        return 0;
+        int run_test(int args, char* argv[], hittable* scene){
+            //TODO
+        };
     }
     
-};
+}
